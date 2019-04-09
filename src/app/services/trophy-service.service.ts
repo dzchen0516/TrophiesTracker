d20a925201ca750787acbfdb649a3bab301a3141
dpeters <dustinpete@gmail.com>
2019-04-09 18:59:17 -0500

    Have the client app talking to the server.

--- src/app/services/trophy-service.service.ts
+++ src/app/services/trophy-service.service.ts
@@ -5,32 +5,50 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class TrophyServiceService {
-  url = 'http://159.203.15.30/trophyapi/api/users/token';
+	url = 'http://159.203.15.30/trophyapi/api/users/token';
+	constructor(private http : HttpClient) { }
 
-  constructor(private http : HttpClient) { }
+	userLogin(username : string, password : string) {
+  
+		//setup header
 
-  userLogin(username : string, password : string) {
-    //setup header
-    var headers = new HttpHeaders();
-    headers.append('Access-Control-Allow-Origin', '*');
-    headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
-    const httpOptions = { headers };
-    
-    //prepare data
-    let body = {
-      username: username,
-      password: password
-    };
+		const httpOptions = { headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				'Accept': 'application/json',
+			})
+		};
 
-    //post data
-    this.http.post(this.url, JSON.stringify(body), httpOptions)
-    .subscribe( //subscribe 
-      data => {
-        console.log(data['_body'])
-      }, error => {;
-        console.log(error)
-      });
-  }
+		let body = {
+			email:"dpeters@avant.ca",
+			password: "admin"
+		}
+
+		//post data
+		this.http.post(this.url, JSON.stringify(body), httpOptions)
+		.subscribe( //subscribe 
+			data => {
+				console.log(data['_body'])
+			}, error => {
+				console.log(error)
+		});
+
+
+		// GET
+		//var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMwLCJleHAiOjE1NTQ5MzU3MjJ9.MajcYOYFJzBmTznZk33U9fugC_Wg0X3-CcmdfMU2dvE";
+
+		/*const httpOptions = { headers: new HttpHeaders({
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + token
+			})
+		};
+
+		this.http.get("http://159.203.15.30/trophyapi/api/users", httpOptions)
+		.subscribe( //subscribe
+				data => {
+					console.log(data['_body'])
+			}, error => {
+				console.log(error)
+		});*/
+    }
 }
