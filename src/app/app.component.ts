2893606bf42adb351616662c3405ab80c8f7d5b8
johnson <johnsonsonchan@hotmail.com>
2019-04-13 17:03:06 -0500

    Disabled auth-guard for now for testing purpose

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,19 +31,19 @@ export class AppComponent {
       //trophyService, if the authenticationState changed this
       //will redirect the user to different page according to the
       //lastest state
-      this.trophyService.authenticationState.subscribe(state => {
-          console.log("trophyService: state changed: ", state);
-          if(state) {
+      //this.trophyService.authenticationState.subscribe(state => {
+          //console.log("trophyService: state changed: ", state);
+          //if(state) {
             //if the user has been authenticated
             //it will redirect the user to the scoreboard page
-            this.router.navigate(['trophy', 'scoreboard']);
-          }
-          else {
+            //this.router.navigate(['trophy', 'scoreboard']);
+          //}
+          //else {
             //if the user has not been authenticated
             //it will redirect the user to the login page
-            this.router.navigate(['login']);
-          }
-      });
+            //this.router.navigate(['login']);
+          //}
+      //});
     });
   }
 }
