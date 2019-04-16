9f61c6cbb8d4859cc0608fb9385f777d84e73c94
johnson <johnsonsonchan@hotmail.com>
2019-04-15 20:39:13 -0500

    Implementing a validator on email field on login page

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TrophyServiceService } from '../services/trophy-service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +10,15 @@ import { TrophyServiceService } from '../services/trophy-service.service';
 })
 export class LoginPage implements OnInit {
 
+  loginForm: FormGroup;
   results: Observable<any>;
   email: '';
   password: '';
-  constructor(private trophyService: TrophyServiceService) { }
+  constructor(private trophyService: TrophyServiceService, public formBuilder: FormBuilder) { 
+    this.loginForm = formBuilder.group({
+      emailInput: ['', Validators.compose([Validators.pattern['+@+'], Validators.required])]
+    });
+  }
 
   ngOnInit() {
   }
