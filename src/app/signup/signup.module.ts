2d5f113fae6acde9aff8d4f2ff1bf5377eb4da52
dpeters <dustinpete@gmail.com>
2019-04-23 22:20:16 -0500

    UNDOING MY STUPID ASS FIRST ATTEMPT AT AN IMAGE PICKER OMG

--- src/app/signup/signup.module.ts
+++ src/app/signup/signup.module.ts
@@ -8,15 +8,6 @@ import { IonicModule } from '@ionic/angular';
 import { SignupPage } from './signup.page';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { HttpClientModule } from '@angular/common/http';
- 
-import { Camera } from '@ionic-native/camera/ngx';
-import { File } from '@ionic-native/file/ngx';
-import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { FilePath } from '@ionic-native/file-path/ngx';
- 
-import { IonicStorageModule } from '@ionic/storage';
-
 const routes: Routes = [
   {
     path: '',
@@ -30,16 +21,8 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
 	ReactiveFormsModule,
-    RouterModule.forChild(routes),
-	HttpClientModule,
-    IonicStorageModule.forRoot()
+    RouterModule.forChild(routes)
   ],
-  declarations: [SignupPage],
-  providers: [
-    Camera,
-    File,
-    WebView,
-    FilePath
-  ]
+  declarations: [SignupPage]
 })
 export class SignupPageModule {}
--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,21 +1,9 @@
-import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TrophyServiceService } from '../services/trophy-service.service';
 import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 import { UsernameValidator } from  '../validators/username';
-
-import { Camera, CameraOptions, PictureSourceType } from '@ionic-native/camera/ngx';
-import { ActionSheetController, ToastController, Platform, LoadingController } from '@ionic/angular';
-import { File, FileEntry } from '@ionic-native/file/ngx';
-import { HttpClient } from '@angular/common/http';
-import { WebView } from '@ionic-native/ionic-webview/ngx';
-import { Storage } from '@ionic/storage';
-import { FilePath } from '@ionic-native/file-path/ngx';
-
-import { finalize } from 'rxjs/operators';
- 
-const STORAGE_KEY = 'my_image';
 
 @Component({
 	 selector: 'app-signup',
@@ -28,130 +16,20 @@ export class SignupPage implements OnInit {
 	public signupForm: FormGroup;
 	public submitAttempt: boolean = false;
 	
-	img = {};
-	
-	constructor(private trophyService: TrophyServiceService, public formBuilder: FormBuilder,
-	private webview: WebView, 
-	private toastController: ToastController, 
-	private filePath: FilePath, 
-	private storage: Storage, 
-	private camera: Camera, 
-	private file: File, 
-	private actionSheetController: ActionSheetController,
-	private platform: Platform,
-	private ref: ChangeDetectorRef)
+	constructor(private trophyService: TrophyServiceService, public formBuilder: FormBuilder)
 	{ 
 		this.signupForm = formBuilder.group({
 	        email: ['', Validators.compose([Validators.required])],
 	        username: ['', Validators.compose([UsernameValidator.isValid, Validators.required])],
 			password: ['', Validators.compose([Validators.required])],
 			confrimPassword: ['', ],
-			avatar: ['',]
+			avatar: ['', Validators.compose([Validators.required])]
 	    });
 	}
 
 	ngOnInit() 
 	{
 	  
-	}
-	
-	async selectImage() {
-		const actionSheet = await this.actionSheetController.create({
-			header: "Select Image source",
-			buttons: [{
-					text: 'Load from Library',
-					handler: () => {
-						this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
-					}
-				},
-				{
-					text: 'Cancel',
-					role: 'cancel'
-				}
-			]
-		});
-		await actionSheet.present();
-	}
-	
-	takePicture(sourceType: PictureSourceType) {
-		var options: CameraOptions = {
-			quality: 100,
-			sourceType: sourceType,
-			saveToPhotoAlbum: false,
-			correctOrientation: true
-		};
-	 
-		this.camera.getPicture(options).then(imagePath => 
-		{
-			if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) 
-			{
-				
-				this.filePath.resolveNativePath(imagePath)
-					.then(filePath => {
-						let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
-						let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
-						this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
-					});
-			}
-		});
-	}
-	
-	createFileName() {
-		var d = new Date(),
-			n = d.getTime(),
-			newFileName = n + ".jpg";
-		return newFileName;
-	}
-	 
-	copyFileToLocalDir(namePath, currentName, newFileName) {
-		this.file.copyFile(namePath, currentName, this.file.dataDirectory, newFileName).then(success => {
-			this.updateStoredImages(newFileName);
-		}, error => {
-			this.presentToast('Error while storing file.');
-		});
-	}
-	 
-	updateStoredImages(name) {
-		this.storage.get(STORAGE_KEY).then(img => {
-			let arr = JSON.parse(img);
-			if (!arr) {
-				let newImage = [name];
-				this.storage.set(STORAGE_KEY, JSON.stringify(newImage));
-			} else {
-				arr.push(name);
-				this.storage.set(STORAGE_KEY, JSON.stringify(arr));
-			}
-	 
-			let filePath = this.file.dataDirectory + name;
-			let resPath = this.pathForImage(filePath);
-	 
-			let newEntry = {
-				name: name,
-				path: resPath,
-				filePath: filePath
-			};
-	 
-			this.img = newEntry;
-			this.ref.detectChanges(); // trigger change detection cycle
-		});
-	}
-
-	pathForImage(img) {
-		if (img === null) {
-			return '';
-		} else {
-			let converted = this.webview.convertFileSrc(img);
-			return converted;
-		}
-	}
- 
-	async presentToast(text) {
-		const toast = await this.toastController.create({
-		message: text,
-		position: 'bottom',
-		duration: 3000
-		});
-		toast.present();
 	}
 	
 	signUp()
@@ -171,7 +49,7 @@ export class SignupPage implements OnInit {
 				this.signupForm.controls.email.value, 
 				this.signupForm.controls.username.value, 
 				this.signupForm.controls.password.value, 
-				'');
+				this.signupForm.controls.avatar.value);
 		}
 	}
 }
