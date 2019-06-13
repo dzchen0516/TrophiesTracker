765a7a4d5b217a5e57c50370ecc1ebbb91b790c2
johnson <johnsonsonchan@hotmail.com>
2019-06-12 20:25:49 -0500

    working on how scoreboard will be updated as the tab changed

--- src/app/trophy/scoreboard/scoreboard.page.ts
+++ src/app/trophy/scoreboard/scoreboard.page.ts
@@ -16,6 +16,9 @@ export class ScoreboardPage implements OnInit {
   static allTrophies;
   static allUsersTrophies;
   results;
+  trophiesDict;
+  currentTrophy;
+  currentPeriod;
 
 
   constructor(private trophyService: TrophyServiceService) {
@@ -28,17 +31,15 @@ export class ScoreboardPage implements OnInit {
     ScoreboardPage.allTrophies = [];
     ScoreboardPage.allUsersTrophies = [];
     self.results = [];
+    self.trophiesDict = {"notes" : 1, "trophy" : 2, "bells": 3};
+    self.currentTrophy = "trophy";
+    self.currentPeriod = "quarter";
 
     this.trophyService.getUsers(ScoreboardPage.allUsersResults, self.setAllUsers);
     this.trophyService.getTrophies(ScoreboardPage.allTrophiesResults, self.setAllTrophies);
     this.trophyService.getUsersTrophies(ScoreboardPage.allUsersTrophiesResults, self.setAllUsersTrophies, self);
   }
 
-  printVar() {
-    console.log(ScoreboardPage.allUsersResults);
-    console.log(ScoreboardPage.allTrophiesResults);
-    console.log(ScoreboardPage.allUsersTrophiesResults);
-  }
 
   ngOnInit() {
 
@@ -59,6 +60,7 @@ export class ScoreboardPage implements OnInit {
   }
 
   setAllUsersTrophies(scoreboard) {
+    scoreboard.results = [];
     ScoreboardPage.allUsersTrophiesResults.forEach(item => {
       //retrive user name from allUsers array
       var userName = ScoreboardPage.allUsers.find(obj => {
@@ -76,6 +78,8 @@ export class ScoreboardPage implements OnInit {
           return obj.username === userName.username;
       });
 
+      console.log(userName.username);
+
       //create new entry {username: number of trophies} 
       //and push it to the results array
       //if the user not existis in the to the results array
@@ -88,6 +92,54 @@ export class ScoreboardPage implements OnInit {
       }
 
     });
+  }
+
+  trophySegmentChanged(ev: any) {
+    console.log('Trophy Segment changed', ev);
+    var self = this;
+    self.currentTrophy = ev.detail.value;
+    var trophyType = self.trophiesDict[self.currentTrophy];
+    self.results = [];
+
+    ScoreboardPage.allUsersTrophiesResults.forEach(item => {
+      //retrive user name from allUsers array
+      var userName = ScoreboardPage.allUsers.find(obj => {
+          return obj.id === item.user_id;
+      });
+
+      console.log(userName);
+
+      //retrive trophy type from allTrophies array
+      var num = ScoreboardPage.allTrophies.filter(obj => {
+          return obj.id === item.trophy_id && item.trophy_id === trophyType;
+      });
+
+      console.log(num);
+
+      //check if the user exisits in the results array
+      //that'll be rendered
+      var keyIndex = self.results.findIndex(obj => {
+          return obj.username === userName.username;
+      });
+
+      //create new entry {username: number of trophies} 
+      //and push it to the results array
+      //if the user not existis in the to the results array
+      if(keyIndex === -1) {
+        self.results.push({username: userName.username, num: num.length});
+      }
+      //otherwise update the number of trohpies for the user
+      else {
+        self.results[keyIndex].num += num.length;
+      }
+
+    });
+  }
+
+  periodSegmentChanged(ev: any) {
+    console.log('Period Segment changed', ev);
+    var self = this;
+    self.currentPeriod = ev.detail.value;
   }
 
 }
