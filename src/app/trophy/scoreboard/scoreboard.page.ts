4a94a9ab552d365a90d3332a2a40e9e63e5c550f
johnson <johnsonsonchan@hotmail.com>
2019-06-15 16:50:24 -0500

    Refactoring scoreboard list rendering algm

--- src/app/trophy/scoreboard/scoreboard.page.ts
+++ src/app/trophy/scoreboard/scoreboard.page.ts
@@ -60,7 +60,8 @@ export class ScoreboardPage implements OnInit {
   }
 
   setAllUsersTrophies(scoreboard) {
-    scoreboard.results = [];
+    scoreboard.renderScoreboard('trophy', 'quarter');
+    /*scoreboard.results = [];
     ScoreboardPage.allUsersTrophiesResults.forEach(item => {
       //retrive user name from allUsers array
       var userName = ScoreboardPage.allUsers.find(obj => {
@@ -91,14 +92,17 @@ export class ScoreboardPage implements OnInit {
         scoreboard.results[keyIndex].num += 1;
       }
 
-    });
+    });*/
   }
 
   trophySegmentChanged(ev: any) {
     console.log('Trophy Segment changed', ev);
     var self = this;
     self.currentTrophy = ev.detail.value;
-    var trophyType = self.trophiesDict[self.currentTrophy];
+
+    self.renderScoreboard(self.currentTrophy, self.currentPeriod);
+    
+    /*var trophyType = self.trophiesDict[self.currentTrophy];
     self.results = [];
 
     ScoreboardPage.allUsersTrophiesResults.forEach(item => {
@@ -133,13 +137,130 @@ export class ScoreboardPage implements OnInit {
         self.results[keyIndex].num += num.length;
       }
 
-    });
+    });*/
   }
 
   periodSegmentChanged(ev: any) {
     console.log('Period Segment changed', ev);
     var self = this;
     self.currentPeriod = ev.detail.value;
+
+    self.renderScoreboard(self.currentTrophy, self.currentPeriod);
+
+    /*var trophyType = self.trophiesDict[self.currentTrophy];
+    self.results = [];
+
+    var today_date = new Date();
+    var this_quarter = self.quarterOfTheYear(today_date.getMonth() + 1);
+
+    ScoreboardPage.allUsersTrophiesResults.forEach(item => {
+      var trophy_date = new Date(item.date_awarded);
+
+      var trophy_quarter = self.quarterOfTheYear(trophy_date.getMonth() + 1);
+
+      console.log(trophy_quarter);
+      console.log(this_quarter);
+
+      //retrive user name from allUsers array
+      var userName = ScoreboardPage.allUsers.find(obj => {
+          return obj.id === item.user_id;
+      });
+
+      console.log(userName);
+
+      //retrive trophy type from allTrophies array
+      var num = ScoreboardPage.allTrophies.filter(obj => {
+          return obj.id === item.trophy_id && item.trophy_id === trophyType;
+      });
+
+      console.log(num);
+
+      //check if the user exisits in the results array
+      //that'll be rendered
+      var keyIndex = self.results.findIndex(obj => {
+          return obj.username === userName.username;
+      });
+
+      //create new entry {username: number of trophies} 
+      //and push it to the results array
+      //if the user not existis in the to the results array
+      if(keyIndex === -1) {
+        self.results.push({username: userName.username, num: num.length});
+      }
+      //otherwise update the number of trohpies for the user
+      else {
+        self.results[keyIndex].num += num.length;
+      }
+
+    });*/
+    
+  }
+
+  quarterOfTheYear(month) {
+      if(month <= 8 && month >= 6) return 1;
+      else if(month >= 9 && month <= 11) return 2;
+      else if(month == 12 || month <= 2) return 3;
+      else if(month >= 3 && month <= 5) return 4;
+  }
+
+  renderScoreboard(currentTrophy, currentPeriod) {
+    console.log("rednering scoreboard");
+    var self = this;
+    var trophyType = self.trophiesDict[currentTrophy];
+    self.results = [];
+
+    var today_date = new Date();
+    var this_quarter = self.quarterOfTheYear(today_date.getMonth() + 1);
+    //var this_quarter = 4;
+
+    //initiate the scoreboard list
+    ScoreboardPage.allUsers.forEach(user => {
+      self.results.push({username: user.username, num: 0});
+    });
+
+    ScoreboardPage.allUsersTrophiesResults.forEach(item => {
+      console.log(this_quarter);
+      var trophy_date = new Date(item.date_awarded);
+      var trophy_quarter = self.quarterOfTheYear(trophy_date.getMonth() + 1);
+      //applied period or time filter
+      if(currentPeriod === 'alltime' || 
+         (currentPeriod === 'quarter' && this_quarter === trophy_quarter)) {
+          //retrive user name from allUsers array
+          var userName = ScoreboardPage.allUsers.find(obj => {
+            return obj.id === item.user_id;
+          });
+
+          console.log(userName);
+
+          //retrive trophy type from allTrophies array
+          var num = ScoreboardPage.allTrophies.filter(obj => {
+            return obj.id === item.trophy_id && item.trophy_id === trophyType;
+          });
+
+          console.log(num);
+
+          //check if the user exisits in the results array
+          //that'll be rendered
+          var keyIndex = self.results.findIndex(obj => {
+            return obj.username === userName.username;
+          });
+
+          //create new entry {username: number of trophies} 
+          //and push it to the results array
+          //if the user not existis in the to the results array
+          if(keyIndex === -1) {
+            self.results.push({username: userName.username, num: num.length});
+          }
+          //otherwise update the number of trohpies for the user
+          else {
+            self.results[keyIndex].num += num.length;
+          }
+      }
+    });
+
+    //sort the scoreboard list in descending order
+    self.results.sort((a, b) => (a.num < b.num) ? 1 : -1);
+
   }
 
 }
